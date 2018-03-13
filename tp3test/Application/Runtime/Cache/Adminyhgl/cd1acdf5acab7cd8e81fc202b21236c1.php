f95e2569274b50a11e4232380a43b8d1d105d73a
banana killer <1274765441@qq.com>
2018-03-13 10:47:36 +0800

    Revert "111"
    This reverts commit 0b7835531a86ba5cd7fb5cb9b338827b5d884819.

--- tp3test/Application/Runtime/Cache/Adminyhgl/cd1acdf5acab7cd8e81fc202b21236c1.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); if($is_super_admin || in_array(1, $userAuthority)): ?><a href="#dashboard-menu" class="nav-header" data-toggle="collapse"><i class=""></i>用户管理</a>
-<ul id="dashboard-menu" class="nav nav-list collapse in">
-	<li ><a href="<?php echo U('User/index');?>">用户列表</a></li>
-	<li ><a href="<?php echo U('Role/index');?>">角色列表</a></li>		
-	<li ><a href="<?php echo U('XueTang/index');?>">学堂列表</a></li>	
-	<li ><a href="<?php echo U('Cate/index');?>">学堂分类</a></li>	
-</ul><?php endif; ?>
